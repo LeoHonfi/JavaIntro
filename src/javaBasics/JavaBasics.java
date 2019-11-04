b01ee32f94633f191408f2d4b7cc043efc63dfb4
leo.honfi <leo.honfi@AMS-GYM-EDV1-14.gym.ams.local>
2019-11-04 18:28:52 +0100

    Java Basics Berechnungen

--- src/javaBasics/JavaBasics.java
+++ src/javaBasics/JavaBasics.java
@@ -7,10 +7,25 @@ public class JavaBasics {
 		System.out.print(" ");
 		System.out.print("du");
 		System.out.println("!");
+		System.out.println(" ");
 
-		
 		System.out.println(120);
+		System.out.println(" ");
+		
+		//R echnen
+		System.out.println("Berechnungen:");
+		System.out.println(" ");
 		System.out.println(2+2);
+		//rechnen 2.0
+		System.out.println(9/3);
+		System.out.println(8/3);
+		System.out.println(8/3.0);
+		//F/D Verwenden
+		System.out.println(9/3F);
+		System.out.println(9D/3);
+		//
+		System.out.println("1+1=" + 1 + 1);
+		System.out.println("1+1=" + (1 + 1));
 	}
 
 }
