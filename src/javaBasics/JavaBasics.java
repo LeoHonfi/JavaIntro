4bbd71ceda8d140fda41d860ce42bcfcd7f8e3e8
leo.honfi <leo.honfi@AMS-GYM-EDV1-14.gym.ams.local>
2019-11-04 18:18:04 +0100

    Java Basics 1

---
+++ src/javaBasics/JavaBasics.java
@@ -0,0 +1,16 @@
+package javaBasics;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		System.out.print("Hallo");
+		System.out.print(" ");
+		System.out.print("du");
+		System.out.println("!");
+
+		
+		System.out.println(120);
+		System.out.println(2+2);
+	}
+
+}
