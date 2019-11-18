33c6dd5c9009c00269d63cacb540089d6784aa53
leo.honfi <leo.honfi@AMS-GYM-EDV1-14.gym.ams.local>
2019-11-18 18:04:53 +0100

    Java 3

---
+++ src/aufgaben/Java3.java
@@ -0,0 +1,23 @@
+package aufgaben;
+
+public class Java3 {
+
+	public static void main(String[] args) {
+	int x = 1;
+	for (;x<=100;) {
+	
+	
+	if((x%2) == 0) {
+		
+	}
+	else {
+		System.out.println(x);
+	}
+x=x+1;
+	}
+	}
+
+	
+	}
+
+
