64fefd984ce475b83d590218a6e00abc7825f68d
leo.honfi <leo.honfi@AMS-GYM-EDV1-14.gym.ams.local>
2019-11-18 18:11:46 +0100

    Java4

---
+++ src/aufgaben/Java4.java
@@ -0,0 +1,21 @@
+package aufgaben;
+
+public class Java4 {
+
+	public static void main(String[] args) {
+	int x = 1;
+	for (;x<=100;) {
+	
+	
+	if((x%2) == 0) {
+		System.out.println(x);
+	}
+	else {
+		
+	}
+x=x+1;
+	}
+	}
+
+	
+	}
